1f1cbbc94f54a8ad680b239216ddf1af2e8f8b64
gsogaming <gokselserkanoguz@gmail.com>
2023-08-07 23:40:36 +0300

    Story Telling Started

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -8,15 +8,46 @@ public class Interact : MonoBehaviour
 {
     private TextWriter.TextWriterSingle textWriterSingle;
 
+    [Header("Dialogue")]
     [SerializeField] GameObject inGameDialogueBox;
-    [SerializeField] GameObject InteractedObjectUI;
-
-
     [SerializeField] TextMeshProUGUI inGameDialogueText;
+    [TextArea(3, 10)] // The parameters define the minimum and maximum lines for the text area
+    [SerializeField] string dialogue;
+    
+    [Header("In Game Tutorials, pop ups etc.")]
     [SerializeField] TextMeshProUGUI inGameTutorialsText;
+    [SerializeField] GameObject InteractedObjectUI;
     [SerializeField] TextMeshProUGUI interactedObjectUIText;
+    [TextArea(3, 10)] // The parameters define the minimum and maximum lines for the text area
+    [SerializeField] string interactionMessage;
+    [TextArea(3, 10)] // The parameters define the minimum and maximum lines for the text area
+    [SerializeField] string inGameTutorialMessage;
+
+    [Header("Text Writer Settings")]
+    [SerializeField] float writeSpeed;
 
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inGameTutorialsText.gameObject.SetActive(true);
+            inGameTutorialsText.text = inGameTutorialMessage;
+            inGameDialogueBox.SetActive(true);
+            textWriterSingle = TextWriter.AddWriter_Static(inGameDialogueText, dialogue, writeSpeed, true, true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inGameTutorialsText.gameObject.SetActive(false);
+            inGameDialogueBox.SetActive(false);
+        }
+    }
 
 
 }
